6865b46165b0cc64768f4fc32c93d4fa071a8097
conlan <conlan2222@gmail.com>
2026-07-24 19:46:49 -0600

    Added Enemy Hypersonic and glitches (on the radar)

--- Src/Gameplay/Radar/RadarObj/EnemyHypersonic/EnemyHypersonic.gd
+++ Src/Gameplay/Radar/RadarObj/EnemyHypersonic/EnemyHypersonic.gd
@@ -3,8 +3,6 @@ extends RadarObject
 
 
 func _special_ready() -> void:
-	position.x += (randf()-0.5) * 2 * 20
-	position.y += (randf()-0.5) * 2 * 20
 	movement_angle_deg = rad_to_deg((position).angle())-90
 	_update_move_vector()
 	
